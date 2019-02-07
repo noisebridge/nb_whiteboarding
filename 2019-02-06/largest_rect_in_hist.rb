60efcb8ef1a9602f0866645c9e6fd6555c9d94a8
ggorlen <gsgorlen@gmail.com>
2019-02-07 15:40:00 -0800

    shorten some output code

--- 2019-02-06/largest_rect_in_hist.rb
+++ 2019-02-06/largest_rect_in_hist.rb
@@ -88,8 +88,7 @@ y.times do |i|
   w.times {|j| hist[i][x+j] = "%"}
 end
 
-puts "\n +" + "-" * (heights.size * 2 - 1) + "+"
-hist.reverse.each {|row| puts " |" + row.join(" ") + "|"}
-puts " +" + "-" * (heights.size * 2 - 1) + "+\n\n"
-puts " [" + heights.join(",") + "]"
+puts "\n +#{"-" * (heights.size * 2 - 1)}+\n"
+hist.reverse.each {|row| puts " |#{row.join " "}|"}
+puts " +#{"-" * (heights.size * 2 - 1)}+\n\n [#{heights.join ","}]"
 puts " largest rect area: #{largest_rectangle_area heights}\n\n"
